7cf4bf54cbbe6c3f80926ddd94e8457f54a289b7
nahku <nsaidy@t-online.de>
2020-04-15 10:53:19 +0200

    fixed main class

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,32 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        //calculate
+        calculate0Error();
     }
 
     public static void calculate0Error(){
-
+        ArrayList<ICalculator> calculators = new ArrayList<ICalculator>();
+        ArrayList<Double> results = new ArrayList<Double>();
         ICalculator iCalculator1 = new Calculator1(1,1,operand.PLUS);
         ICalculator iCalculator2 = new Calculator2(1,1, operand.PLUS);
-        ICalculator iCalculator3;
-        ICalculator iCalculator4; //= MyClass1(1,2,"+");
-        ArrayList<Float> results;
+        ICalculator iCalculator3 = new Calculator3(1,1, operand.PLUS);
+        ICalculator iCalculator4= new Calculator4(1,1, operand.PLUS);
+        calculators.add(iCalculator1);
+        calculators.add(iCalculator2);
+        calculators.add(iCalculator3);
+        calculators.add(iCalculator4);
 
+        for(int i = 0; i < 4; i++)
+        {
+            results.add(calculators.get(i).getResult());
+        }
 
-        //vote(results);
-
+        vote(results);
     }
 
-    public static void vote(ArrayList<Float> results){
-        Set<Float> uniques = new HashSet<Float>();
-        float result = 0;
+    public static void vote(ArrayList<Double> results){
+        Set<Double> uniques = new HashSet<Double>();
+        double result = 0;
         int counter = 0;
         for (int i = 0; i<4;i++){
 
@@ -32,7 +39,7 @@ public class Main {
             }
         }
 
-        int deviations = counter--;
+        int deviations = --counter;
 
         switch (deviations){
             case 0:
@@ -43,8 +50,10 @@ public class Main {
                 break;
             case 2:
                 System.out.println("2 Fehler");
+                break;
             case 3:
                 System.out.println("Keine übereinstimmenden Ergebnisse");
+                break;
             default:
                 break;
         }
